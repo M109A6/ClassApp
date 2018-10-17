5c227249cfef807159c040009bc44fd10a4640f4
Adam Gonzales <Adam@gonz.cloud>
2018-10-17 08:59:41 -0700

    10-17 update 1
    10-17 update 1

--- ClothingApp/ClothingApp/Account.cs
+++ ClothingApp/ClothingApp/Account.cs
@@ -27,9 +27,30 @@ namespace ClothingApp
     }
     class GuestAccount
     {
+        #region Properties
+        /// <summary>
+        /// User account properties
+        /// </summary>
         public string GuestAccountEmail { get; set; }
         public string GuestAccountAddress { get; set; }
         public string GuestAccountPayment { get; set; }
+
+        #endregion
+    }
+
+    class Clothing
+    {
+        #region Properties
+        /// <summary>
+        /// Clothing Item Properties
+        /// </summary>
+
+        public string ClothingType { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public decimal Quantity { get; set; }
+
+        #endregion
     }
 
     #region Methods
